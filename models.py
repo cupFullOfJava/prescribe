74a52b8a5d7f46a593fb8042d0c2d0c1c46c7098
mitchpowell1 <mdp9648@uncw.edu>
2016-04-10 20:00:24 -0400

    added models file

---
+++ models.py
@@ -0,0 +1,25 @@
+from peewee import *
+
+database = MySQLDatabase('prescribe', **{'password': 'prescribeapppass', 'user': 'prescribeapp'})
+
+class UnknownField(object):
+    pass
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Users(BaseModel):
+    user_pw = CharField(null=True)
+    username = CharField(primary_key=True)
+
+    class Meta:
+        db_table = 'users'
+
+class Searches(BaseModel):
+    artist = DecimalField(db_column='artist_id', primary_key=True)
+    username = ForeignKeyField(db_column='username', rel_model=Users, to_field='username', unique=True)
+
+    class Meta:
+        db_table = 'searches'
+
