8e3881c63db11e41dcafc5e1f81b9af14d31b35f
mitchpowell1 <mdp9648@uncw.edu>
2016-04-10 20:33:39 -0400

    ported the application to Flask

--- SiteCode/application.py
+++ SiteCode/application.py
@@ -5,5 +5,32 @@ associated mysql database.
 
 """
 
-application = 
+from flask import *
+from models import *
 
+application = Flask(__name__)
+application.config["DEBUG"] = True
+
+
+@application.route('/')
+def home_page():
+    return render_template('Home.html')
+
+
+@application.route('/about/')
+def about_page():
+    return render_template('About.html')
+
+
+@application.route('/login/')
+def login_page():
+    return render_template('login.html')
+
+
+@application.route('/register/')
+def registration_page():
+    return render_template('Register.html')
+
+
+if __name__ == '__main__':
+    application.run()
